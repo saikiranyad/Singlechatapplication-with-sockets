95a19ebed59f212140108490218ac4afc4bf0dc9
saikiranyad <saisaikiran7890@gmail.com>
2025-05-22 19:28:17 +0530

    removingthconsoles

--- backend/utils/cloudinary.js
+++ backend/utils/cloudinary.js
@@ -5,6 +5,6 @@ cloudinary.config({
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
 });
-console.log(cloudinary.config());
+
 
 module.exports = cloudinary;
